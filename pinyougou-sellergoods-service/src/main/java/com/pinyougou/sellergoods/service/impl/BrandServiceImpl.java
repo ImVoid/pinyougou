1f3b21509f8a6975eef2453f9806ab3d53bd13ba
ken <793743423@qq.com>
2018-11-08 21:31:30 +0800

    Initial commit

---
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,25 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.mapper.TbBrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @author liuweijian
+ * @version 2018/11/8
+ */
+@Service
+public class BrandServiceImpl implements BrandService {
+
+	@Autowired
+	private TbBrandMapper brandMapper;
+
+	@Override
+	public List<TbBrand> findAll() {
+		return brandMapper.selectByExample(null);
+	}
+}
