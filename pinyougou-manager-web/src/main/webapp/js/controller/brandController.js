812f1ee51b9049ea1989b8de20e7acd1c55d1257
ken <793743423@qq.com>
2018-12-05 15:36:01 +0800

    前段代码集成实现

--- pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
+++ pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
@@ -1,32 +1,14 @@
 // 品牌控制层
-app.controller('brandController', function ($scope, $http, brandService) {
+app.controller('brandController', function ($scope, $http, $controller, brandService) {
+
+    //集成baseController
+    $controller('baseController', {$scope:$scope});
 
     //查询品牌列表
     $scope.findAll = function () {
         brandService.findAll().success(function (response) {
             $scope.list = response;
         })
-    }
-
-    // 分页控件配置
-    $scope.paginationConf = {
-        // 当前页
-        currentPage: 1,
-        // 总记录数
-        totalItems: 10,
-        // 每页记录数
-        itemsPerPage: 10,
-        // 分页选项
-        perPageOptions: [10, 20, 30, 40, 50],
-        // 页码变动时进行的操作
-        onChange: function () {
-            $scope.reloadList();
-        }
-    }
-
-    // 重新加载品牌列表
-    $scope.reloadList = function() {
-        $scope.search($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
     }
 
     // 分页查询品牌列表 TODO 作废，使用search替代
@@ -75,20 +57,6 @@ app.controller('brandController', function ($scope, $http, brandService) {
         brandService.findOne(id).success(function (response) {
             $scope.entity = response;
         })
-    }
-
-    // 保存选中的选项ID
-    $scope.selectIds = [];
-    // 实时更新选中选项
-    $scope.updateSelection = function ($event, id) {
-        //如果被选中
-        if ($event.target.checked) {
-            $scope.selectIds.push(id);
-        } else {	//取消选中
-            var index = $scope.selectIds.indexOf(id)
-            //开始删除位置，删除个数
-            $scope.selectIds.splice(index, 1)
-        }
     }
 
     // 删除品牌
