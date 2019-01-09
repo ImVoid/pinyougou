dae8a722b0eafad1c71200c7a913f089240c841c
ken <793743423@qq.com>
2019-01-09 11:34:31 +0800

    （mapper path add /）

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -77,7 +77,7 @@ public class BrandController {
 		}
 	}
 
-	@RequestMapping("selectOptionList")
+	@RequestMapping("/selectOptionList")
 	public List<Map> selectOptionList() {
 		return brandService.selectOptionList();
 	}
